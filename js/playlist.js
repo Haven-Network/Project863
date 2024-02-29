3a99672f0a51a7c4ff94822e055f08d63b401c8b
tay <149682424+xJustTay@users.noreply.github.com>
2024-02-29 02:47:09 -0500

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,14 @@
+let songs = [
+  {
+    name: "The Apple II",
+    artist: "Project 863",
+    img: "863",
+    audio: "music1"
+  },
+  {
+    name: "This Is Only the Beginning",
+    artist: "Project 863",
+    img: "863",
+    audio: "music2"
+  },
+]
